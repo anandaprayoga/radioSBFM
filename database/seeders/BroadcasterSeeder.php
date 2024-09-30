a80447dd2fa6002d1fea643beae1c2ca72a15dcc
MDR-CAD <frelancedesaign@gmail.com>
2024-09-30 20:41:58 +0700

    perbaiki seeder jadwal siaran

--- database/seeders/BroadcasterSeeder.php
+++ database/seeders/BroadcasterSeeder.php
@@ -17,6 +17,7 @@ class BroadcasterSeeder extends Seeder
     {
         DB::table('broadcaster')->insert([
         [
+            'id' => 1,
             'nama_broadcaster' => 'A. Raihan Gimnastiar',
             'no_hp' => '08817555811',
             'tanggal_bergabung' => now(),
@@ -24,6 +25,7 @@ class BroadcasterSeeder extends Seeder
             'updated_at' => now()
         ],
         [
+            'id' => 2,
             'nama_broadcaster' => 'A. Makmun Alji',
             'no_hp' => '08828666922',
             'tanggal_bergabung' => now(),
@@ -31,6 +33,7 @@ class BroadcasterSeeder extends Seeder
             'updated_at' => now()
         ],
         [
+            'id' => 3,
             'nama_broadcaster' => 'Makmun Alji',
             'no_hp' => '08828666923',
             'tanggal_bergabung' => now(),
@@ -38,6 +41,7 @@ class BroadcasterSeeder extends Seeder
             'updated_at' => now()
         ],
         [
+            'id' => 4,
             'nama_broadcaster' => 'Alji',
             'no_hp' => '08828666924',
             'tanggal_bergabung' => now(),
@@ -45,26 +49,13 @@ class BroadcasterSeeder extends Seeder
             'updated_at' => now()
         ],
         [
+            'id' => 5,
             'nama_broadcaster' => 'Makmun A.',
             'no_hp' => '08828666925',
             'tanggal_bergabung' => now(),
             'created_at' => now(),
             'updated_at' => now()
-        ],
-        [
-            'nama_broadcaster' => 'Almun',
-            'no_hp' => '08828666926',
-            'tanggal_bergabung' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
-        [
-            'nama_broadcaster' => 'Ananda',
-            'no_hp' => '08828666927',
-            'tanggal_bergabung' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ],
+        ]
         ]);
     }
 }
