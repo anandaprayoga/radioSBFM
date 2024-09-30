19082a813fb6e7952166115a67734a55faa9e388
MDR-CAD <frelancedesaign@gmail.com>
2024-09-30 21:38:07 +0700

    mengubah style

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -51,39 +51,14 @@
         </div>
       </div>
     </div>
-
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-      <div class="card">
-        <div class="card-body p-3">
-          <div class="row">
-            <div class="col-8">
-              <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Pendapatan Hari Ini</p>
-                <h5 class="font-weight-bolder mb-0">
-                  Rp0
-                  <span class="text-danger text-sm font-weight-bolder">+0%</span>
-                </h5>
-              </div>
-            </div>
-            <div class="col-4 text-end">
-              <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                <!-- <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i> -->
-                <i class="fa-solid fa-money-bills text-lg opacity-10"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
-
   <div class="row mt-4">
-    <div class="col-lg-5 mb-lg-0 mb-4">
+    <div class="col-lg mb-lg-0 mb-4">
       <div class="card z-index-2">
         <div class="card-body p-3">
           <div class="bg-gradient-dark border-radius-lg py-3 pe-1 mb-3">
             <div class="chart">
-              <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
+              <canvas id="chart-bars" class="chart-canvas" height="300"></canvas>
             </div>
           </div>
           <h6 class="ms-2 mt-4 mb-0"> Active Users </h6>
@@ -190,22 +165,6 @@
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-7">
-      <div class="card z-index-2">
-        <div class="card-header pb-0">
-          <h6>Sales overview</h6>
-          <p class="text-sm">
-            <i class="fa fa-arrow-up text-success"></i>
-            <span class="font-weight-bold">4% more</span> in 2021
-          </p>
-        </div>
-        <div class="card-body p-3">
-          <div class="chart">
-            <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
           </div>
         </div>
       </div>
